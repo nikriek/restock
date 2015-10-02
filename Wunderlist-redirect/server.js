e48832edcf243d68528513db7eff085abbfd4720
Carl Julius Gödecken <carl.goedecken@student.hpi.de>
2015-10-03 00:25:18 +0200

    Add basic Wunderlist login (incomplete)

--- Wunderlist-redirect/server.js
+++ Wunderlist-redirect/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
@@ -10,7 +11,6 @@ app.get('/redirect', function(req, res){
         res.code(500).end('No code found!');
     }   else    {
         //// Get the access token
-        //var request = require('request');
         //request({url: 'http://www.wunderlist.com/oauth/access_token', form:{client_id:'f7dff4e1225dc1459172',
         //        client_secret:'73868615a4151c7445be60745a3cb35a3c6b5c138879f1da74d187aaf07e',
         //        code: code}},
@@ -27,6 +27,48 @@ app.get('/redirect', function(req, res){
             'window.location = "restock://' + code + '"' +
             '</script></head><body></body></html>');
     }
+
+});
+
+app.get('/listId', function (req, res) {
+var accesstoken = req.query.access_token;
+if(accesstoken == undefined)    {
+    res.end('No access token detected');
+    return;
+}
+request({url: 'http://a.wunderlist.com/api/v1/lists', headers: {
+    'X-Access-Token': accesstoken,
+    'X-Client-ID': 'f7dff4e1225dc1459172'}, json: true}, function (error, response, body) {
+        var groceryListId;
+        if (!error && response.statusCode == 200) {
+            var array = response.body
+            for(var i in array) {
+                console.log(array[i].title);
+                if (array[i].title == "Groceries") {
+                    groceryListId = array[i].id;
+                    console.log('Found id ' + groceryListId);
+                    break;
+                }
+            }
+            if(groceryListId == undefined)    {
+                // create a new list
+                request({url: 'a.wunderlist.com/api/v1/lists', headers: {
+                    'X-Access-Token': accesstoken,
+                    'X-Client-ID': 'f7dff4e1225dc1459172'}, form: {name: "Restock-Groceries"}}, function (err2, res2, body2) {
+                    //groceryListId = res2.body.id;
+                    //console.log('Created new list with id %s', body2.id);
+                    if(error)   console.log(error);
+                    console.log(body2);
+                })
+            }
+
+
+            res.end(JSON.stringify({id: groceryListId}));
+        }   else {
+            console.log('Error (Status ' + response.statusCode + '): ' + error);
+        }
+    });
+    // {
 
 });
 
--- iOS/Restock/WunderlistClient.swift
+++ iOS/Restock/WunderlistClient.swift
@@ -7,8 +7,42 @@
 //
 
 import UIKit
+import Alamofire
 
 class WunderlistClient: NSObject {
+    
+    var groceryListId : String
+    var accessToken : String
+    
+    let loginParameters = [X-Access-Token: accessToken, X-Client-ID: Constants.WunderlistClientId]
+    
+    func findGroceriesList()    {
+        Alamofire.request(.GET, "http://restock.thinkcarl.com/listId", parameters: loginParameters)
+            .responseJSON { response in
+                if let idString = response["id"], id = Int(idString)    {
+                    groceryListId = id
+                }   else {
+                    print("Error parsing list id")
+                }
+            }
+    }
+    
+    func login()    {
+        UIApplication.sharedApplication().openURL("https://www.wunderlist.com/oauth/authorize?client_id=f7dff4e1225dc1459172&redirect_uri=http://restock.thinkcarl.com/redirect&state=NOTRANDOM")
+    }
+    
+    func completeLogin(code: String)    {
+        let parameters = ["client_id": Constants.WunderlistClientId,
+        "client_secret": Constants.WunderlistClientSecret,
+        "code": code]
+        Alamofire.request(.POST, "https://www.wunderlist.com/oauth/access_token", parameters: parameters).responseJSON  {
+            response in
+            if let token = response["access_token"] as? String  {
+                accessToken = token
+            }
+        }
+    }
+    
     func addProduct(product: Product) {
         
     }
