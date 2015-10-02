94546571ef66fa9c6b01433af48c7ab58d5ed57f
Carl Julius Gödecken <carl.goedecken@student.hpi.de>
2015-10-03 00:25:53 +0200

    Merge

--- iOS/Restock/Constants.swift
+++ iOS/Restock/Constants.swift
@@ -12,10 +12,7 @@ struct Constants {
     static let GridWidth : CGFloat = 8
     static let GridHeight : CGFloat = 24
     static let ScanditBarcodeScannerAppKey = "DTnmLAOp91sca/0zi96hHkz7a7miKpPIh16NA5yidMc"
-<<<<<<< Updated upstream
     static let EandataKeycode = "C710A16E560EF5B8"
-=======
     static let WunderlistClientId = "f7dff4e1225dc1459172"
     static let WunderlistClientSecret = "73868615a4151c7445be60745a3cb35a3c6b5c138879f1da74d187aaf07e"
->>>>>>> Stashed changes
 }
