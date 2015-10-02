70ad8c545b2363a29d67e9757145e3cc159ec31f
Carl Julius GoÃàdecken <carl.goedecken@student.hpi.de>
2015-10-02 16:58:42 +0200

    Add Setup screen UI and other stuff

--- iOS/Restock/AppDelegate.swift
+++ iOS/Restock/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.makeKeyAndVisible()
+        
+        self.window?.rootViewController = SetupViewController()
+        
         return true
     }
 
