6193e6cfa2c1bd86b8b0e6d58333a9b6048ff96e
Daniel Thevessen <danthe96@gmx.de>
2015-10-02 23:54:36 +0200

    Implemented adding products to Wunderlist.

--- Android/Restock/app/src/main/java/io/restock/android/MainActivity.java
+++ Android/Restock/app/src/main/java/io/restock/android/MainActivity.java
@@ -2,22 +2,40 @@ package io.restock.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.mirasense.scanditsdk.ScanditSDKAutoAdjustingBarcodePicker;
 import com.mirasense.scanditsdk.interfaces.ScanditSDK;
+import com.mirasense.scanditsdk.interfaces.ScanditSDKCode;
 import com.mirasense.scanditsdk.interfaces.ScanditSDKOnScanListener;
 import com.mirasense.scanditsdk.interfaces.ScanditSDKScanSession;
+
+import org.json.JSONObject;
+
+import java.util.List;
 
 /**
  * Created by danth on 10/2/2015.
  */
 public class MainActivity extends Activity implements ScanditSDKOnScanListener {
 
-    private static final String APP_KEY = "DTnmLAOp91sca/0zi96hHkz7a7miKpPIh16NA5yidMc";
-
     private ScanditSDK barcodePicker;
+
+    public static RequestQueue requestQueue;
+
+    private ScannedItemsController itemsController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +47,27 @@ public class MainActivity extends Activity implements ScanditSDKOnScanListener {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         ScanditSDKAutoAdjustingBarcodePicker specificPicker = new ScanditSDKAutoAdjustingBarcodePicker(
-                this, APP_KEY, ScanditSDKAutoAdjustingBarcodePicker.CAMERA_FACING_BACK);
+                this, Constants.SCANDIT_APP_KEY, ScanditSDKAutoAdjustingBarcodePicker.CAMERA_FACING_BACK);
 
         // Set our specific type of picker as content, cast up for later.
         setContentView(specificPicker);
         barcodePicker = specificPicker;
 
         barcodePicker.addOnScanListener(this);
+
+        // inflate custom overlay and add it to Scandit's overlay
+        RelativeLayout defaultOverlay = (RelativeLayout) barcodePicker.getOverlayView();
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View customOverlay = inflater.inflate(R.layout.scandit_custom_overlay, null);
+        defaultOverlay.addView(customOverlay);
+
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.start();
+
+        int wunderlist_list_id = -1;
+        // TODO init wunderlist and get "groceries" list
+
+        itemsController = new ScannedItemsController(this, wunderlist_list_id);
 
     }
 
@@ -55,8 +87,27 @@ public class MainActivity extends Activity implements ScanditSDKOnScanListener {
     @Override
     public void didScan(ScanditSDKScanSession session) {
 
-        session.getNewlyDecodedCodes();
+        List<ScanditSDKCode> codes = session.getNewlyDecodedCodes();
+        if (codes != null && codes.size() > 0) {
+            ScanditSDKCode selectedCode = codes.get(codes.size() - 1);
 
+            if (itemsController != null) {
+                JsonObjectRequest productRequest = new JsonObjectRequest(Request.Method.GET,
+                        "",
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // TODO
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                            }
+                        });
+            }
+
+        }
 
     }
 
---
+++ Android/Restock/app/src/main/java/io/restock/android/Product.java
@@ -0,0 +1,19 @@
+package io.restock.android;
+
+/**
+ * Created by danth on 10/2/2015.
+ */
+public class Product {
+
+    private String upc;
+    private String name;
+
+
+    public String getUPC() {
+        return upc;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
